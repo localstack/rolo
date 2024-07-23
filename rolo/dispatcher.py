eb8617b46a5b94cad8f9978abbb1ba1157cc8dd3
Thomas Rausch <thomas@thrau.at>
2024-07-23 16:01:35 +0200

    fix pydantic type hint check

--- rolo/dispatcher.py
+++ rolo/dispatcher.py
@@ -1,8 +1,9 @@
 """
 DEPRECATED: use ``from rolo.routing import handler_dispatcher`` instead
 """
-from .routing.handler import handler_dispatcher
+from .routing.handler import ResultValue, handler_dispatcher
 
 __all__ = [
     "handler_dispatcher",
+    "ResultValue",
 ]
